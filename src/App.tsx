93406482dd57b5626dd8971d7e6ac7f965323413
Keith Davidson <davidson.keith@gmail.com>
2023-10-17 10:30:28 +1000

    Create 2 simple date timers, one using plain HTML/JS in index.html, and one using React in App.tsx. Shows advantages of dealing with partial page updates using React

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [myDate, myDateUpdate] = useState(new Date());
+
+  useEffect(() => {
+    const intervalID = setInterval(() => {
+      myDateUpdate(new Date());
+    }, 1000);
+    return () => clearInterval(intervalID);
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Example React</h3>
+      <input/>
+      <div>${myDate.toString()}</div>
     </div>
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.tsx</code> and save to reload!!
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
