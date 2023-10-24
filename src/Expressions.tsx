c4c06e233e4111fe8c6115e1e7615af6798a91e4
Keith Davidson <davidson.keith@gmail.com>
2023-10-24 14:28:37 +1000

    Playing with React Fragments

---
+++ src/Expressions.tsx
@@ -0,0 +1,9 @@
+export default function Expressions(){
+  const subtext = 'This is my subtext';
+  return(
+    <>
+      <h1>Expressions example</h1>
+      <h2>{subtext}</h2>
+    </>
+  )
+}
--- src/Simple.tsx
+++ src/Simple.tsx
@@ -1,16 +1,12 @@
 // import { jsx as _jsx } from "react/jsx-runtime";
 // import { jsxs as _jsxs } from "react/jsx-runtime";
-
-
-import React from "react";
+// import React from "react";
 
 export default function Simple() {
   return (
-    React.createElement("div", {
-      myatt: "myVal"
-    }, React.createElement("h1", null, "hi..."))
-  //   <div>
-  //     <h1>Hi mate</h1>
-  //   </div>
+    <div>
+      <h1>Hi mate!</h1>
+      <h2>Up and rolling...</h2>
+    </div>
   )
 }
