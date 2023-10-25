fa6c441c7b7781a9f761b09a0861f0a7230700bf
Keith Davidson <davidson.keith@gmail.com>
2023-10-25 15:17:27 +1000

    Playing with State and conditionals

---
+++ src/ConditionalsIf.tsx
@@ -0,0 +1,47 @@
+import {useState} from "react";
+import Simple from "./Simple";
+// import {valueOf} from "react-scripts";
+
+export default function ConditionalsIf() {
+  const [rating, setRating] = useState(5);
+
+  const displayResult = ()=>{
+    if(rating === 1){
+      return <span>Nooo!</span>
+    } else if(rating === 2){
+      return (
+        <>
+          <span>What, why?</span>
+          <input type="text"/>
+        </>
+      )
+    } else if(rating === 3){
+      return (
+        <Simple/>
+      )
+    } else if(rating === 4){
+      return <span>I'll try harder next time</span>
+    } else if(rating === 5){
+      return <span>Yesss!!!  </span>
+    }
+  }
+
+  return (
+    <>
+      <h1>Conditionals If Example</h1>
+      <div>Rate this site</div>
+      <select value={rating} onChange={(e) => {
+        console.log(e.currentTarget.value)
+        setRating(parseInt(e.currentTarget.value));
+      }}>
+        <option value={1}>1</option>
+        <option value={2}>2</option>
+        <option value={3}>3</option>
+        <option value={4}>4</option>
+        <option value={5}>5</option>
+      </select>
+      <div>Your rating: {rating}</div>
+      <div>{displayResult()}</div>
+    </>
+  )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Expressions from "./Expressions";
-import Conditionals from "./Conditionals";
-import Events from "./Events";
+import ConditionalsIf from "./ConditionalsIf";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Events/>
+    <ConditionalsIf/>
   </React.StrictMode>
 );
 
